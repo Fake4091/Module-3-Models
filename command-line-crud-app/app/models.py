43100c23254b3fb93c0ea778132b3c6a1ab2c33e
Fake4091 <aswends7517@gmail.com>
2024-10-18 10:12:24 -0500

    Finished crud

--- command-line-crud-app/app/models.py
+++ command-line-crud-app/app/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    is_starred = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.is_starred:
+            return f"{'\033[1m'}* Title:{'\033[0m'} {self.title}\n{'\033[1m'}Desc:{'\033[0m'} {self.description}\n{40*'-'}"
+        else:
+            return f"{'\033[1m'}Title:{'\033[0m'} {self.title}\n{'\033[1m'}Desc:{'\033[0m'} {self.description}\n{40*'-'}"
+
+
+def new_note(title, description, is_starred=False):
+    note = Note(title=title, description=description, is_starred=is_starred)
+    note.save()
+    return note
+
+
+def view_notes():
+    notes = Note.objects.all()
+
+    return notes
+
+
+def search_notes_by_title(search):
+    try:
+        note = Note.objects.get(title=search)
+        return note
+    except Note.DoesNotExist:
+        return None
+    except Note.MultipleObjectsReturned:
+        return None
+
+
+def starred_notes():
+    notes = Note.objects.filter(is_starred=True)
+
+    return notes
+
+
+def star_unstar_note(title):
+    try:
+        note = Note.objects.get(title=title)
+        note.is_starred = not note.is_starred
+        note.save()
+        return note
+    except Note.DoesNotExist:
+        return None
+
+
+def update_note(title, new_description):
+    try:
+        note = Note.objects.get(title=title)
+        note.description = new_description
+        note.save()
+        return note
+    except Note.DoesNotExist:
+        return None
+
+
+def delete_note(title):
+    try:
+        note = Note.objects.get(title=title)
+        note.delete()
+        return True
+    except Note.DoesNotExist:
+        return None
--- command-line-crud-app/app/tests.py
+++ command-line-crud-app/app/tests.py
@@ -1,3 +1,148 @@
 from django.test import TestCase
+from app import models
 
 # Create your tests here.
+
+
+class TestNotes(TestCase):
+    def test_create_note(self):
+        note = models.new_note("Test", "test")
+
+        self.assertEqual(note.id, 1)
+        self.assertEqual(note.title, "Test")
+        self.assertEqual(note.description, "test")
+        self.assertFalse(note.is_starred)
+
+    def test_view_all_notes(self):
+        notes_data = [
+            {
+                "title": "Test1",
+                "description": "test",
+                "is_starred": False,
+            },
+            {
+                "title": "Test2",
+                "description": "test",
+                "is_starred": True,
+            },
+            {
+                "title": "Test3",
+                "description": "test",
+                "is_starred": False,
+            },
+        ]
+
+        for note in notes_data:
+            models.new_note(note["title"], note["description"], note["is_starred"])
+
+        notes = models.view_notes()
+
+        notes_data = sorted(notes_data, key=lambda n: n["title"])
+        notes = sorted(notes, key=lambda n: n.title)
+
+        for data, note in zip(notes_data, notes):
+            self.assertEqual(data["title"], note.title)
+            self.assertEqual(data["description"], note.description)
+            self.assertEqual(data["is_starred"], note.is_starred)
+
+    def test_can_search_title(self):
+        notes_data = [
+            {
+                "title": "Test1",
+                "description": "test",
+                "is_starred": False,
+            },
+            {
+                "title": "Test2",
+                "description": "huzzah",
+                "is_starred": True,
+            },
+            {
+                "title": "Test3",
+                "description": "test",
+                "is_starred": False,
+            },
+        ]
+
+        for note in notes_data:
+            models.new_note(note["title"], note["description"], note["is_starred"])
+
+        self.assertIsNone(models.search_notes_by_title("aousnth"))
+
+        note = models.search_notes_by_title("Test2")
+
+        self.assertIsNotNone(note)
+        self.assertEqual(note.description, "huzzah")
+
+    def test_can_view_starred(self):
+        notes_data = [
+            {
+                "title": "Test1",
+                "description": "test",
+                "is_starred": True,
+            },
+            {
+                "title": "Test2",
+                "description": "test",
+                "is_starred": False,
+            },
+            {
+                "title": "Test3",
+                "description": "test",
+                "is_starred": True,
+            },
+        ]
+
+        for note_data in notes_data:
+            models.new_note(
+                note_data["title"],
+                note_data["description"],
+                note_data["is_starred"],
+            )
+
+        self.assertEqual(len(models.starred_notes()), 2)
+
+    def test_can_star_unstar_note(self):
+        note = models.new_note("Test", "test")
+        note = models.star_unstar_note("Test")
+
+        self.assertEqual(note.is_starred, True)
+
+        note = models.star_unstar_note("Test")
+        self.assertEqual(note.is_starred, False)
+
+    def test_can_update_note(self):
+        note = models.new_note("Test", "test")
+        note = models.update_note("Test", "test2")
+
+        self.assertEqual(note.description, "test2")
+
+    def test_can_delete_note(self):
+        notes_data = [
+            {
+                "title": "Test1",
+                "description": "test",
+                "is_starred": True,
+            },
+            {
+                "title": "Test2",
+                "description": "test",
+                "is_starred": False,
+            },
+            {
+                "title": "Test3",
+                "description": "test",
+                "is_starred": True,
+            },
+        ]
+
+        for note_data in notes_data:
+            models.new_note(
+                note_data["title"],
+                note_data["description"],
+                note_data["is_starred"],
+            )
+
+        models.delete_note("Test1")
+
+        self.assertEqual(len(models.view_notes()), 2)
